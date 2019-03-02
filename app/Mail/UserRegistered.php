e46e1da9f181a62ff1b1f7883ed795f127c23ca7
Caesar Ali L <caesaralilamondo@gmail.com>
2019-03-02 14:17:49 +0800

    .

--- app/Mail/UserRegistered.php
+++ app/Mail/UserRegistered.php
@@ -7,7 +7,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class UserRegistered extends Mailable
+class UserRegistered extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
