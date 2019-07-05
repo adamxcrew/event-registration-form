0dc52f02816f3dd42ea613026b3b8e1d91838085
Caesar Ali L <caesaralilamondo@gmail.com>
2019-07-05 22:53:57 +0800

    Fix invoice.

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -14,8 +14,8 @@ class InvoiceController extends Controller
         $registration = Registration::where('code', $code)->first();
         if ($registration) {
             $pdf = PDF::loadView('reports.invoice', compact('registration'))->setPaper('A4');
-            return $pdf->stream();
-            // return $pdf->download('invoice-' . $registration->code . '.pdf');
+            // return $pdf->stream();
+            return $pdf->download('invoice-' . $registration->code . '.pdf');
         }
         abort(404);
     }
