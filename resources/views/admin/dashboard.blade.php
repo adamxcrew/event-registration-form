5e28927bdbfb2911251207cf7c6bcec4da846691
Caesar Ali L <caesaralilamondo@gmail.com>
2021-06-26 15:28:23 +0800

    wip

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -56,18 +56,15 @@
         <div class="card">
             <div class="card-header border-bottom-0 d-flex">
                 <h5 class="card-title d-inline-block text-muted">Event Information</h5>
-                <a href="#" class="text-muted ml-auto">
+                <a href="{{ route('config.index') }}" class="text-muted ml-auto">
                     <i class="fas fa-cog ml-auto"></i>
                 </a>
             </div>
             <div class="card-body bg-light">
                 <strong><i class="fa fa-calendar-day mr-1"></i> Event</strong>
                 <p class="text-muted">
-                    {{ config('app.desc') }}
+                    {{ eventInfo('about') }}
                 </p>
-                <hr>
-                <strong><i class="fas fa-map-marker-alt mr-1"></i>Time & Place</strong>
-                <p class="text-muted">-</p>
             </div>
         </div>
     </div>
